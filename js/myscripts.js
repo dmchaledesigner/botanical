f4af25fead70a545659651b66bb105de72ba0b95
dmchaledesigner <dmchaledesigner@gmail.com>
2016-02-14 15:42:23 +0800

    first commit

---
+++ js/myscripts.js
@@ -0,0 +1,124 @@
+$(window).load(function(){
+
+
+  // niceScroll
+  $("html").niceScroll();
+    
+    
+  // Stick menu
+  $(".menu").sticky({topSpacing:0});
+
+
+
+
+  // Menu Scroll to content and Active menu
+  var lastId,
+    topMenu = $("#menu"),
+    topMenuHeight = topMenu.outerHeight()+145,
+    menuItems = topMenu.find("a"),
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+
+   $('a[href*=#]').bind('click', function(e) {
+	e.preventDefault();
+       
+	var target = $(this).attr("href");
+			
+
+	$('html, body').stop().animate({ scrollTop: $(target).offset().top-50 }, 1000, function() {
+
+	});
+			
+	return false;
+   });
+
+  $(window).scroll(function(){
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   
+   if (lastId !== id) {
+       lastId = id;
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href=#"+id+"]").parent().addClass("active");
+   }                   
+  });  
+  
+
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    
+    $(".footer").css( "position", "relative" );
+    $(".contact").css( "marginBottom", "0" );
+
+}
+else 
+{
+
+  // FadeTo elements
+  if ( $(window).width() > 1023) {  
+
+    //tiles = $("h2, h3, .container, .contact, .form, .content .contact-text ").fadeTo(0, 0);
+
+        tiles = $(".text, .menu, #about-us, #look-around, #design, #architects, #gallery h1, #builder, #quality").fadeTo(0, 0);
+
+
+    $(window).scroll(function(d,h) {
+      tiles.each(function(i) {
+          a = $(this).offset().top + $(this).height();
+          b = $(window).scrollTop() + $(window).height();
+          if (a < b) $(this).fadeTo(1000,1);
+      });
+    });
+
+  }
+
+}
+
+
+
+  //Menu mobile click
+  $( ".icon" ).click(function() {
+    $( " ul.menu-click" ).slideToggle( "slow", function() {
+    // Animation complete.
+    });
+  });
+
+
+
+
+  // Parallax
+  if ($('.parallax-background').length) {
+    $(".parallax-background").parallax();
+  }
+  
+  // Parallax
+  if ($('.parallax-background-partners').length) {
+    $(".parallax-background-partners").parallax();
+  }  
+
+
+  // Parallax
+  if ($('.parallax-background-quote').length) {
+    $(".parallax-background-quote").parallax();
+  }  
+
+  // Parallax
+  if ($('.parallax-background-architects').length) {
+    $(".parallax-background-architects").parallax();
+  }  
+
+
+// Parallax
+  if ($('.parallax-background-quality').length) {
+    $(".parallax-background-quality").parallax();
+  }  
+
+});
